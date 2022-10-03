b429e45a1a6658a106cde2513d50ae574d632340
Arthur <arthurmafra.mg@gmail.com>
2022-10-03 12:40:51 -0300

    Acerto do enunciado em README.md

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        double n1, n2;
+
+        System.out.println("Digite dois numeros: ");
+        n1 = sc.nextDouble();
+        n2 = sc.nextDouble();
+        System.out.println("A soma dos dois numeros e: " + (n1 + n2));
+
+        sc.close();
     }
 }
